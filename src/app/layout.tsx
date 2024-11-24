83c22ef6ac6fdb167a6dfd9054b70e511aae8037
Taku-Sashima <131934396+Taku-Sashima@users.noreply.github.com>
2024-11-24 14:02:07 +0900

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import '@/styles/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <head>{/* 必要に応じてメタタグやタイトルをここに追加 */}</head>
+      <body>
+        <nav>
+          <Link href={'/'}>ホーム</Link>
+          <Link href={'/EuclidPattern'}>ユークリッド</Link>
+        </nav>
+        {children}
+      </body>
+    </html>
+  );
+}
